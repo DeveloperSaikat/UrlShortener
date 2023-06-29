869de8cd3e0ac59e4a3a5fc7593b39f22bad0916
DeveloperSaikat <saikatk594@gmail.com>
2023-06-29 19:53:10 +0530

    Added all the files

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const shortUrl = require('./models/shortUrl');
+const app = express();
+
+const MongoDBUri = 'mongodb+srv://saikatdevworks:cJQjt0HhOoxexSf6@test-cluster.kfa5qnw.mongodb.net/urls?w=majority'
+
+mongoose.connect(MongoDBUri, {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', async (req, res) => {
+    const allUrls = await shortUrl.find()
+    res.render('index', {allUrls: allUrls});   
+});
+
+app.post('/shortUrl', async (req, res) => {
+    await shortUrl.create({ original: req.body.originalUrl })
+    res.redirect('/');
+});
+
+app.get('/:inputUrl', async (req, res) => {
+    const retrievedUrl = await shortUrl.findOne({ short: req.params.inputUrl});
+    if (retrievedUrl === null) return res.sendStatus(404)
+    res.redirect(retrievedUrl.original); 
+})
+
+app.listen(process.env.POST || 5000);
